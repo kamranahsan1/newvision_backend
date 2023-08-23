921b3825af4b896d03fc825ad52a467e689c15e7
kamranahsan1 <kamranahsan75@gmail.com>
2023-08-23 13:09:44 +0400

    server

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./db/database");
 const insertData = require("./models/Data/Insert");
-
+const port = process.env.PORT || 5000;
 dotenv.config({ path: "backend/config/config.env" });
+connectDatabase();
+insertData();
 
-const startServer = async () => {
-  await connectDatabase();
-  await insertData();
-  const server = app.listen(process.env.PORT, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log(`Server is working on http://${host}:${port}`);
-    console.log(server);
-  });
-};
-
-startServer();
+const server = app.listen(port, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Server is working on http://${host}:${port}`);
+});
