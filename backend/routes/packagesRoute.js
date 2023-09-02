a135b7e0fa9a68e3a521b462d71d2c9567a7407d
kamranahsan1 <kamranahsan75@gmail.com>
2023-09-02 13:30:12 +0400

    Update packagesRoute.js

--- backend/routes/packagesRoute.js
+++ backend/routes/packagesRoute.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { getAllPackages, getCategories } = require("../controller/Packages");
 const router = express.Router();
 
-console.log(getAllPackages);
-console.log(getCategories);
 //router.route("/packages").get(getAllPackages);
 //router.route("/categories").get(getCategories);
 
