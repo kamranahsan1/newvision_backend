4d8f6220a209c80c861eef19de63edf0ed58c453
kamranahsan1 <kamranahsan75@gmail.com>
2023-09-02 12:57:32 +0400

    fix

--- backend/controller/Country.js
+++ backend/controller/Country.js
@@ -2,10 +2,19 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Country = require("../models/Country");
 const NodeCache = require("node-cache");
 
+const cache = new NodeCache();
+
 const getCountries = catchAsyncErrors(async (req, res, next) => {
-  const countries = await Country.find();
-  cache.set(cacheKey, countries);
-  res.status(200).json(countries);
+  const cacheKey = "countries";
+  const cachedCountries = cache.get(cacheKey);
+
+  if (cachedCountries) {
+    res.status(200).json(cachedCountries);
+  } else {
+    const countries = await Country.find();
+    cache.set(cacheKey, countries);
+    res.status(200).json(countries);
+  }
 });
 
 module.exports = {
