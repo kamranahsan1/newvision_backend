02772783b8908dd4b639564b73121f79acedec2a
kamranahsan1 <kamranahsan75@gmail.com>
2023-09-02 13:53:26 +0400

    chk

--- backend/controller/packages.js
+++ backend/controller/packages.js
@@ -1,10 +1,6 @@
-const ApiFeatures = require("../utils/ApiFeatures");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const Packages = require("../models/Packages");
-const Category = require("../models/Category");
-const NodeCache = require("node-cache");
 
-const cache = new NodeCache();
 const getPackages = catchAsyncErrors(async (req, res, next) => {
   let { category } = req.query;
   category = category || "Uae Packages";
@@ -13,40 +9,6 @@ const getPackages = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json(packages);
 });
 
-const getAllPackages = catchAsyncErrors(async (req, res, next) => {
-  let { resultPerPage } = req.query;
-  resultPerPage = resultPerPage || 12;
-
-  const apiFeatures = new ApiFeatures(Packages.find(), req.query)
-    .search()
-    .filter()
-    .pagination(resultPerPage);
-
-  const packages = await apiFeatures.query;
-  const packagesCount = packages.length;
-
-  res.status(200).json({
-    status: true,
-    packages,
-    packagesCount,
-    resultPerPage,
-  });
-});
-
-const getCategories = catchAsyncErrors(async (req, res, next) => {
-  const cacheKey = "categories";
-  const cachedCategories = cache.get(cacheKey);
-  if (cachedCategories) {
-    res.status(200).json(cachedCategories);
-  } else {
-    const categories = await Category.find();
-    cache.set(cacheKey, categories);
-    res.status(200).json(categories);
-  }
-});
-
 module.exports = {
   getPackages,
-  getAllPackages,
-  getCategories,
 };
