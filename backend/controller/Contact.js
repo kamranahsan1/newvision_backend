b58368d9686b4da397ad8ca287afb35bdbc69f48
kamranahsan1 <kamranahsan75@gmail.com>
2023-09-06 15:45:14 +0400

    Update Contact.js

--- backend/controller/Contact.js
+++ backend/controller/Contact.js
@@ -43,28 +43,34 @@ const saveContact = catchAsyncErrors(async (req, res, next) => {
 const saveSubscriber = catchAsyncErrors(async (req, res, next) => {
   try {
     const { email } = req.body;
+    const existingSubscriber = await Subscriber.findOne({ email });
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!email && !emailRegex.test(email)) {
-      return res.status(400).json({ message: "Invalid email format" });
-    }
-
-    const newSubscriber = new Subscriber({ email });
-
-    const validationError = newContact.validateSync();
-    if (validationError) {
-      const errors = {};
-      for (const field in validationError.errors) {
-        errors[field] = validationError.errors[field].message;
+    if (existingSubscriber) {
+      existingSubscriber.status = "active";
+      await existingSubscriber.save();
+      return res.status(200).json({ message: "Subscriber is now active!" });
+    } else {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!email && !emailRegex.test(email)) {
+        return res.status(400).json({ message: "Invalid email format" });
       }
-      return res.status(400).json({ errors });
+
+      const newSubscriber = new Subscriber({ email });
+
+      const validationError = newSubscriber.validateSync();
+      if (validationError) {
+        const errors = {};
+        for (const field in validationError.errors) {
+          errors[field] = validationError.errors[field].message;
+        }
+        return res.status(400).json({ errors });
+      }
+
+      await newSubscriber.save();
+      res.status(200).json({ message: "Subscribed Successfully!" });
     }
-
-    await newSubscriber.save();
-
-    res.status(200).json({ message: "Subscribed Successfully!" });
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error: " + error });
   }
 });
 
