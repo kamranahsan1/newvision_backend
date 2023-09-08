d7d8391c0cc37932ff679ef21c8e976d4315cdf9
kamranahsan1 <kamranahsan75@gmail.com>
2023-09-08 17:52:11 +0400

    Update Contact.js

--- backend/controller/Contact.js
+++ backend/controller/Contact.js
@@ -6,7 +6,7 @@ const saveContact = catchAsyncErrors(async (req, res, next) => {
   try {
     const { name, email, phone, reason, message } = req.body;
 
-    if (!name || !email || !phone || !reason || !message) {
+    if (!name || !email || !phone || !message) {
       return res.status(400).json({ message: "All fields are required" });
     }
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
