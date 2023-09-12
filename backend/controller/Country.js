3da38678ad79e1aece8bc77b40e7a7a33d81cf08
kamranahsan1 <kamranahsan75@gmail.com>
2023-09-12 15:26:20 +0400

    Update Country.js

--- backend/controller/Country.js
+++ backend/controller/Country.js
@@ -5,6 +5,11 @@ const NodeCache = require("node-cache");
 const cache = new NodeCache();
 
 const getCountries = catchAsyncErrors(async (req, res, next) => {
+  const countries = await Country.find();
+  cache.set(cacheKey, countries);
+  res.status(200).json(countries);
+
+  /*
   const cacheKey = "countries";
   const cachedCountries = cache.get(cacheKey);
 
@@ -15,6 +20,7 @@ const getCountries = catchAsyncErrors(async (req, res, next) => {
     cache.set(cacheKey, countries);
     res.status(200).json(countries);
   }
+  */
 });
 
 module.exports = {
